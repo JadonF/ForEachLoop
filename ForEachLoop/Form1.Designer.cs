4445d7266f6328d63802187386e1985d64cfc8bf
JadonF <jadon.fournier@ocsbstudent.ca>
2018-04-11 10:14:48 -0400

    Initial commit

---
+++ ForEachLoop/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ForEachLoop
+{
+    partial class frmForEachLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.btnStart.Location = new System.Drawing.Point(57, 151);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 52);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblTest.Location = new System.Drawing.Point(276, 177);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(112, 26);
+            this.lblTest.TabIndex = 1;
+            this.lblTest.Text = "Test Label";
+            // 
+            // frmForEachLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 354);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmForEachLoop";
+            this.Text = "For...Each Loop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
---
+++ ForEachLoop/Form1.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 11-Apr-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program uses a for...each loop to change the colours on the form
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEachLoop
+{
+    public partial class frmForEachLoop : Form
+    {
+        public frmForEachLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //loop it
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Blue;
+
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Red;
+                }
+            }
+        }
+    }
+}
